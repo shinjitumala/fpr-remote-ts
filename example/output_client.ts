8693befdc8c09135af77003fdc61dd008b7767bc
Shinji Hoshino <funny.pig.run@gmail.com>
2023-04-08 23:53:25 +0900

    Update examples.

--- example/output_client.ts
+++ example/output_client.ts
@@ -1,23 +1,18 @@
 // BEGIN CLIENT HEADER
-import * as http from "http"
 
 const opts = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
 }
-const parse_res_as = <T>(host: string, path: string, obj: Object) => {
+const parse_res_as = async <T>(domain: string, path: string, obj: Object) => {
     return new Promise<T>(p => {
-        var buf: Buffer
-        const req = http.request(
-            `${host}/${path}`,
-            opts,
-            (res) => {
-                res.on("data", (d) => { buf += d })
-                res.on("end", () => { p(JSON.parse(buf.toString()) as T) })
-            }
-        )
-        req.write(JSON.stringify(obj))
-        req.end()
+        fetch(`${domain}/${path}`, {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: { "Content-Type": "application/json; charset=UTF-8" }
+        }).then(r => {
+            p(r.json() as T)
+        })
     })
 }
 // END CLIENT HEADER
--- example/output_server.ts
+++ example/output_server.ts
@@ -8,21 +8,17 @@ export class Cat {
 }
 // END DEFS
 // BEGIN SERVER HEADER
+import * as api from "./api_impl"
 import exp from "express"
 
+const root = __dirname + "/" + root_rel
 const srv = exp()
 srv.use(exp.json())
-
-
-srv.post("/name", (req, res) => {
-    console.log(req.body)
-
-    const res_body = { message: "Not implemented." }
-    res.json(res_body)
-})
-
+srv.use(exp.static(root))
+srv.get('/', (req, res) => { res.sendFile(root + "/index.html") })
 srv.listen(port, () => {
     console.log(`Listening on port ${port}`)
+    console.log(`Access the server at: ${domain}`)
 })
 // END SERVER HEADER
 // BEGIN API
